13c54773e48f7631c3a6e471d32afda73d9290ee
FeudeyTF <yanmordanenko089@gmail.com>
2023-09-22 13:53:52 +0300

    Добавил возведение в степень числа + работа с ОГРОМНЫМИ цифрами

--- BaseWorking/BaseWorking.cs
+++ BaseWorking/BaseWorking.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BaseWorking
+namespace Base
 {
     internal class BaseWorking
     {
         public static string Alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
-        public static string ToBase(long number, int bas)
+        public static string ToBase(BigInteger number, int bas)
         {
             string result = "";
             while (number != 0)
@@ -19,14 +20,22 @@ namespace BaseWorking
             }
             return result;
         }
-        public static long FromBase(string number, int bas)
+        public static BigInteger FromBase(string number, int bas)
         {
-            long result = 0;
+            BigInteger result = 0;
             for(int i = number.Length; i > 0; i--)
-                result += (long)(Alphabet.IndexOf(number[i - 1].ToString().ToLower()) * Math.Pow(bas, number.Length - i));
+                result += (BigInteger)(Alphabet.IndexOf(number[i - 1].ToString().ToLower()) * Math.Pow(bas, number.Length - i));
             return result;
         }
 
         public static string FromToBase(string number, int prevBase, int nextBase) => ToBase(FromBase(number, prevBase), nextBase);
+
+        public static BigInteger Power(int num, int j)
+        {
+            BigInteger result = num;
+            for (int i = 1; i < j; i++)
+                result *= num;
+            return result;
+        }
     }
 }
--- BaseWorking/Program.cs
+++ BaseWorking/Program.cs
@@ -1,1 +1,1 @@
-﻿Console.WriteLine(BaseWorking.BaseWorking.FromToBase("123", 16, 10));
+﻿
